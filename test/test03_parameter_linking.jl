ab34b1aad473b593231da80603de6664b6cd0415
SimonHansul <simonhansul@gmail.com>
2025-06-04 18:02:32 +0200

    WIP param linking test

--- test/test03_parameter_linking.jl
+++ test/test03_parameter_linking.jl
@@ -1,2 +1,22 @@
 #TODO: implement test for linking parameters
 #probably also needed for EcotoxSystems.jl
+
+
+using Pkg; Pkg.activate("test")
+using Test
+using Distributions
+
+
+using Plots, StatsPlots, Plots.Measures
+default(leg = false, lw = 1.5)
+
+include("testutils.jl")
+
+using DataFrames, DataFramesMeta
+using StatsBase
+
+using Revise
+
+@time import AmphiDEB: defaultparams, ODE_simulator, Amphibian_DEB!, AmphiDEB_ODE!
+using AmphiDEB
+using EcotoxSystems
